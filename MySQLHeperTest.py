46e08014f138120bf8b9ab966f317153bc74583e
smile2015 <mwb20088@163.com>
2019-01-19 18:08:34 +0800

    更新Pycharm配置文件Pycharm_settings.jar

--- MySQLHeperTest.py
+++ MySQLHeperTest.py
@@ -24,11 +24,47 @@ if (default_encoding != sys.getdefaultencoding()):
     sys.setdefaultencoding(default_encoding)
 
 '''
-
+MySQLHelper类测试
 '''
 
 mySqlHelper=MySQLHelper()
 mySqlHelper.connetMySQL(host,user,pwd,dbname)
 print  mySqlHelper.conn
 
+sql_insert="INSERT INTO account (name,password) VALUES ('test','test');"
+sql_update="UPDATE account SET password= 'msmiles1' WHERE name = 'test';"
+sql_delete="DELETE FROM account WHERE name = 'test';"
+select_sql="SELECT * FROM account;"
 
+def queryMySQL():
+    # 把一个事务放到一个try块里，如果出现异常就回滚
+
+    try:
+        mySqlHelper.cursor.execute(sql_insert)
+        print mySqlHelper.cursor.rowcount
+
+        mySqlHelper.cursor.execute(sql_update)
+        print mySqlHelper.cursor.rowcount
+
+        mySqlHelper.cursor.execute(sql_delete)
+        print mySqlHelper.cursor.rowcount
+
+        # 提交事务
+        mySqlHelper.conn.commit()
+
+        # 格式化增删改后的数据查出来
+        mySqlHelper.cursor.execute(select_sql)
+        rs = mySqlHelper.cursor.fetchall()
+        for row in rs:
+            print "id=%d, name=%s, password=%s, createtime=%s" % row
+
+        mySqlHelper.closeCursor()
+        mySqlHelper.closeConnet()
+
+    except Exception as e:
+        mySqlHelper.conn.rollback()  # 若有异常就回滚
+        mySqlHelper.closeCursor()
+        mySqlHelper.closeConnet()
+
+if __name__ == '__main__':
+    pass
