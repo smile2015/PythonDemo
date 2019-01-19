60a0812223a582db7bf82080c87e805ca634c84e
smile2015 <mwb20088@163.com>
2019-01-19 15:28:36 +0800

    PropertiesUtils

--- MySQLDBCONSTANT.py
+++ MySQLDBCONSTANT.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 -------------------------------------------------
-   File Name：     MySQLHeperTest
+   File Name：     MySQLDBCONSTANT
    Description :
    Author :       Administrator
    date：          2019/1/19 0019
@@ -10,9 +10,6 @@
                    2019/1/19 0019:
 -------------------------------------------------
 """
-from MySQLDBCONSTANT import *
-from com.mosorg.common.db.MySQLHelper import MySQLHelper
-
 __author__ = 'Administrator'
 
 import sys
@@ -24,9 +21,13 @@ if (default_encoding != sys.getdefaultencoding()):
     sys.setdefaultencoding(default_encoding)
 
 '''
-
+MySQL数据库配置数据
 '''
 
-mySqlHelper=MySQLHelper()
-mySqlHelper.connetMySQL(host,user,pwd,dbname)
-print  mySqlHelper.conn
+#cmd 连接数据库命令：mysql -uroot -p
+host="localhost"
+port=3306
+user="root"
+pwd=""
+dbname="test"
+encoding="utf8"
